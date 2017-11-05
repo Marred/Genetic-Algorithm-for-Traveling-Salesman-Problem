f20bfc73f17e3c9acd6d139ac1f1fd89e0b629d3
Marred <redzimski@hotmail.com>
2017-11-05 23:20:03 +0100

    Separated logic, added option to save results
    Separated logic to different class, added option to save results in a text file

---
+++ PathUtils.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace WdTIGS
+{
+    class PathUtils
+    {
+        private PathUtils() {
+            ReadDistances();
+        }
+
+        public static PathUtils Instance { get { return Nested.instance;  } }
+
+        private class Nested
+        {
+            static Nested() { }
+            internal static readonly PathUtils instance = new PathUtils();
+        }
+
+        static int numOfCities;
+        static int[,] distances;
+        static int[][] instances;
+
+        private static void ReadDistances()
+        {
+            StreamReader reader = new StreamReader(@"C:\berlin52.txt");
+            numOfCities = Int32.Parse(reader.ReadLine());
+            distances = new int[numOfCities, numOfCities];
+
+            for (int i = 0; i < numOfCities; i++)
+            {
+                string line = reader.ReadLine().Trim();
+                string[] values = line.Split(' ');
+                for (int j = 0; j < values.Length; j++)
+                {
+                    distances[i, j] = Int32.Parse(values[j]);
+                }
+            }
+        }
+
+        public void RandomizeInstances(int numberOfInstances)
+        {
+            instances = new int[numberOfInstances][];
+            Random random = new Random();
+            for (int i = 0; i < numberOfInstances; i++)
+            {
+                instances[i] = new int[numOfCities];
+                List<int> list = Enumerable.Range(0, numOfCities).ToList();
+                int temp;
+                for (int j = 0; j < numOfCities; j++)
+                {
+                    temp = random.Next(list.Count);
+                    instances[i][j] = list[temp];
+                    list.RemoveAt(temp);
+                }
+            }
+        }
+
+        public void SaveInstance(int index)
+        {
+            string text = string.Empty;
+            foreach(var instance in instances[index])
+            {
+                text += instance + "-";
+            }
+            text = text.Remove(text.Length - 1, 1);
+            text += " " + GetSumDistance(instances[index]);
+            System.IO.File.WriteAllText(@"C:\Users\redzi\Desktop\odleglosci\result.txt", text);
+        }
+
+        private static int GetSumDistance(int[] instance)
+        {
+            int sum = 0;
+            for (int i = 1; i < instance.Length; i++)
+            {
+                sum += GetSingleDistance(instance[i - 1], instance[i]);
+            }
+            sum += GetSingleDistance(instance[0], instance[instance.Length - 1]);
+            return sum;
+        }
+
+        private static int GetSingleDistance(int a, int b)
+        {
+            return a > b ? distances[a, b] : distances[b, a];
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,78 +4,18 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WdTIGS;
 
 namespace VSCode
 {
     class Program
     {
-        static int[,] arr;
-        static Random random = new Random();
-
         static void Main(string[] args)
         {
-            arr = ReadFromFile();
+            PathUtils utils = PathUtils.Instance;
 
-            int[][] instances = GetRandomInstances(arr.GetLength(0), 40);
-
-            foreach (var instance in instances)
-            {
-                Console.WriteLine(GetSumDistance(instance));
-            }
-
-            Console.ReadKey();
-        }
-
-        private static int[,] ReadFromFile()
-        {
-            StreamReader reader = new StreamReader(@"C:\berlin52.txt");
-            int size = Int32.Parse(reader.ReadLine());
-            int[,] temp = new int[size, size];
-
-            for (int i = 0; i < size; i++)
-            {
-                string line = reader.ReadLine().Trim();
-                string[] values = line.Split(' ');
-                for (int j = 0; j < values.Length; j++)
-                {
-                    temp[i, j] = Int32.Parse(values[j]);
-                }
-            }
-            return temp;
-        }
-
-        private static int[][] GetRandomInstances(int numOfCities, int size)
-        {
-            int[][] arr = new int[size][];
-            for (int i = 0; i < size; i++)
-            {
-                arr[i] = new int[numOfCities];
-                List<int> list = Enumerable.Range(0, 52).ToList();
-                int temp;
-                for (int j = 0; j < numOfCities; j++)
-                {
-                    temp = random.Next(list.Count);
-                    arr[i][j] = list[temp];
-                    list.RemoveAt(temp);
-                }
-            }
-            return arr;
-        }
-
-        private static int GetSumDistance(int[] instance)
-        {
-            int sum = 0;
-            for (int i = 1; i < instance.Length; i++)
-            {
-                sum += GetSingleDistance(instance[i - 1], instance[i]);
-            }
-            sum += GetSingleDistance(instance[0], instance[instance.Length - 1]);
-            return sum;
-        }
-
-        private static int GetSingleDistance(int a, int b)
-        {
-            return a > b ? arr[a, b] : arr[b, a];
+            utils.RandomizeInstances(10);
+            utils.SaveInstance(1);
         }
     }
 }
